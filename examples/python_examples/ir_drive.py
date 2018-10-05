19f0b13a4691400cc7bdbd45516b0e2d18ea79c8
RemBullis <remington.bullis@mines.sdsmt.edu>
2018-10-05 11:53:09 -0600

    Added a note about what packages need to be installed for vision things, and also an example script to drive the wheels with the IR sensor.

---
+++ examples/python_examples/ir_drive.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+
+
+left_pub = rospy.Publisher('/geekbot/left_wheel', Int32, queue_size=10)
+right_pub = rospy.Publisher('/geekbot/right_wheel', Int32, queue_size=10)
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + " IR: %s", data.data)
+    if data.data < 15:
+        msg = Int32()
+        msg.data = 50
+        left_pub.publish(msg)
+        right_pub.publish(msg)
+    else: 
+        msg = Int32()
+        msg.data = 0
+        left_pub.publish(msg)
+        right_pub.publish(msg)
+
+def listener():
+    rospy.init_node('drive_node', anonymous=True)
+
+    rospy.Subscriber("/geekbot/ir_cm", Int32, callback)
+
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
